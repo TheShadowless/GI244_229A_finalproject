25e81d3eb330c8e43659e08aa7b51a69612f2966
TheShadowless <kittipom.ku@gmail.com>
2025-05-12 13:29:53 +0700

    camera offset

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    [SerializeField]private Vector3 offset = new Vector3(0, 5, -10);
+    [SerializeField]private Vector3 offset = new Vector3(3, 3, -12);
+      
 
+    private void Awake()
+    {
+        transform.position = target.position + offset;
+    }
     void LateUpdate()
     {
+        /*
         if (target != null)
         {
             transform.position = target.position + offset;
         }
+        */
     }
 }
 
